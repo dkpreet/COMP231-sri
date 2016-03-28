8a68533fea8ce67ad235e0fa44c9ee5c1dc6ca2e
dkpreet <dkpreet.k@gmail.com>
2016-03-28 14:23:31 -0400

    Sri's project is uploaded

---
+++ Test/Default.aspx
@@ -0,0 +1,56 @@
+﻿
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Test.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:gridview ID="GridView1" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+         <Columns>
+             <asp:TemplateField>
+            <ItemTemplate>
+                <asp:CheckBox ID="chkRow" runat="server" AutoPostBack="true" OnCheckedChanged="ChckedChanged" />
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:BoundField DataField="Course_Code" HeaderText="Course Code" ItemStyle-Width="150" ReadOnly="True" SortExpression="Course_ID" />
+        <asp:BoundField DataField="Course_Number" HeaderText="Course Number" ItemStyle-Width="250" ReadOnly="True" SortExpression="Course_Name" />
+        <asp:BoundField DataField="Course_Name" HeaderText="Course Name" ItemStyle-Width="250" ReadOnly="True" SortExpression="Course_Name" />
+    </Columns>
+    </asp:gridview>
+
+
+
+        <br />
+
+
+          <asp:gridview ID="GridView2" runat="server" AutoGenerateColumns="False">
+         <Columns>
+          <asp:TemplateField>
+            <ItemTemplate>
+                <asp:CheckBox ID="chkRow2" runat="server" AutoPostBack="true" OnCheckedChanged="ChckedChanged" />
+            </ItemTemplate>
+            </asp:TemplateField>
+                     <asp:BoundField DataField="Course_Name" HeaderText="Course Name" ItemStyle-Width="150" ReadOnly="True" />
+                     <asp:BoundField DataField="section_ID" HeaderText="Section" ItemStyle-Width="150" ReadOnly="True" />
+                     <asp:BoundField DataField="Day" HeaderText="Day" ItemStyle-Width="150" ReadOnly="True" />
+                     <asp:BoundField DataField="S_Time" HeaderText="Start" ItemStyle-Width="150" ReadOnly="True" />
+             <asp:BoundField DataField="E_Time" HeaderText="End Time" ItemStyle-Width="150" ReadOnly="True" />       
+    </Columns>
+     </asp:gridview>
+
+        
+
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Show Available Sections" />
+
+        <%--<asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:courseregistrationConnectionString %>" ProviderName="<%$ ConnectionStrings:courseregistrationConnectionString.ProviderName %>" SelectCommand="SELECT * FROM courseregistration.course;"></asp:SqlDataSource>--%>
+        <asp:Button ID="Button2" runat="server" Text="Enroll the Selected Sections" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Test/Default.aspx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace Test
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string sConnectionString = @"server=localhost;user id=root;password=comp231;database=courseregistartion;";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+
+            }
+
+        }
+
+        private void BindGridView()
+        {
+            DataTable dt = new DataTable();
+
+
+            try
+            {
+
+                string sQuery = "SELECT Course_Code, Course_Number, Course_Name FROM courseregistartion.course";
+
+                MySqlConnection con = new MySqlConnection(sConnectionString);
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(sQuery, con);
+                MySqlDataReader sdr = cmd.ExecuteReader();
+
+                dt.Load(sdr);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+                con.Close();
+
+
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                dt.Dispose();
+
+            }
+
+
+        }
+
+        protected void ChckedChanged(object sender, EventArgs e)
+        { 
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            Bindtable();
+
+           
+        }
+
+        private void Bindtable()
+        {
+
+            DataTable dts = new DataTable();
+
+            foreach (GridViewRow row in GridView1.Rows)
+            {
+                if (row.RowType == DataControlRowType.DataRow)
+                {
+                    CheckBox chkRow = (row.Cells[0].FindControl("chkRow") as CheckBox);
+                    if (chkRow.Checked)
+                    {
+                        string CourseCode = row.Cells[1].Text;
+                        string CourseNumber = row.Cells[2].Text;
+
+                        try
+                        {
+                            string sQuery = "SELECT Course_Name, Section_ID, Day, S_Time, E_Time FROM section, course, timeslot WHERE section.Course_Code = course.Course_Code AND section.Course_Number = course.Course_Number AND section.Slot_ID = timeslot.Slot_ID AND course.Course_Code = '" + CourseCode + "' AND course.Course_Number = '" + CourseNumber + "'";
+
+                            MySqlConnection con = new MySqlConnection(sConnectionString);
+                            con.Open();
+                            MySqlCommand cmd = new MySqlCommand(sQuery, con);
+                            MySqlDataReader sdr = cmd.ExecuteReader();
+
+                            dts.Load(sdr);
+                            GridView2.DataSource = dts;
+                            GridView2.DataBind();
+                            con.Close();
+                        }
+                        catch
+                        {
+
+                        }
+
+                        finally
+                        {
+                            dts.Dispose();
+
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+    }
